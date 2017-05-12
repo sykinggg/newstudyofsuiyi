6a18dd3c07ca57327b273f5018908d4037ae4d1a
suiyi2016 <suiyi19920228@qq.com>
2017-05-12 19:02:27 +0800

    2017-05-12 公司 - angular2 路由

--- angular2Study/src/app/app.module.ts
+++ angular2Study/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 //基础用户输入
 import { AppComponent }  from './UseInputComponent/app.component';
@@ -12,6 +13,8 @@ import { keyUp2 } from './UseInputComponent/app.key-up2';
 import { keyEnter } from './UseInputComponent/app.key-up3';
 import { keyBlur } from './UseInputComponent/app.key-blur';
 import { inConclusion } from './UseInputComponent/app.conclusion';
+//基础用户输入路由文件
+import { basicUserInputRouter } from './UseInputComponent/basicUserInputRouter';
 
 //表单
 import { HeroFormComponent } from './angular-from/hero-form.component';
@@ -24,29 +27,37 @@ import { HeroesComponent } from './dependent-injection/heroes.component';
 
 
 import { HeroServer } from './dependent-injection/hero.service';
+
+//路由数组
+const routers: Routes = [
+    //基础用户输入
+    { path: 'basicUserInputRouter', component:  basicUserInputRouter}
+]
+
+
+
 @NgModule({
   //所有组件全部放在这下面包括引用和被应用的
   declarations: [
       AppComponent, ClickMeComponent, keyupComponent_v1,
       loopBackComponent, keyUp2, keyEnter,
       keyEnter, keyBlur, inConclusion,
-      HeroFormComponent, HeroListComponent, HeroesComponent
+      HeroFormComponent, HeroListComponent, HeroesComponent,
+      basicUserInputRouter
   ],
   //本身带有的装饰器
   imports: [
-      BrowserModule, FormsModule, HttpModule
+      BrowserModule, FormsModule, HttpModule,
+      RouterModule.forRoot(routers)
   ],
   //一方面，NgModule 中的服务是被注册到根注入器。这意味着在 NgModule 中注册的服务可以被整个应用访问
   //在应用组件中注册的提供商只在该组件及其子组件中可用
   providers: [
       HeroServer
   ],
-  //可以在页面上渲染的组件
+  //可以在页面上渲染的根组件
   bootstrap: [
-      AppComponent, ClickMeComponent, keyupComponent_v1,
-      loopBackComponent, keyUp2, keyEnter,
-      keyEnter, keyBlur, inConclusion,
-      HeroFormComponent, HeroesComponent
+      HeroFormComponent
   ]
 })
 export class AppModule { }
