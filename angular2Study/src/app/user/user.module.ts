8362273205bbb00c5c4efbf390d591da2dbe9624
suiyi2016 <suiyi19920228@qq.com>
2017-05-31 18:23:55 +0800

    2017-05-31

---
+++ angular2Study/src/app/user/user.module.ts
@@ -0,0 +1,17 @@
+// import { CommonModule } from '@angular/common';
+// import { NgModule } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+// import { ReactiveFormsModule } from '@angular/forms';
+//
+//
+//
+// @NgModule({
+//     declarations: [],
+//     imports: [
+//         CommonModule,
+//         ReactiveFormsModule,
+//
+//     ],
+//     exports: [],
+//     providers: []
+// })
