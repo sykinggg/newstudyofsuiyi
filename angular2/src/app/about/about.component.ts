30bb3cf1e230a8bc3e5a4dd7ea43ddfab7b42f7a
suiyi2016 <suiyi19920228@qq.com>
2017-11-21 18:51:03 +0800

    优化

--- angular2/src/app/about/about.component.ts
+++ angular2/src/app/about/about.component.ts
@@ -5,8 +5,8 @@ import { fadeIn } from '../animations/fade-in';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
-  animation: [ fadeIn ]
+  styleUrls: ['./about.component.scss']
+  // animation: [ fadeIn ]
 })
 export class AboutComponent implements OnInit {
 
