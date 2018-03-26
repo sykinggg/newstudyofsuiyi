5964d740124730cc69fe30b27d13ede847232cc6
suiyi2016 <suiyi19920228@qq.com>
2018-03-26 18:08:14 +0800

    react 2018-03-26

--- react-project/src/compoent/reduxCreateStore.js
+++ react-project/src/compoent/reduxCreateStore.js
@@ -5,22 +5,25 @@ import React, { Component } from 'react';
  * 		创建一个拥有应用程序完整状态树
  * 	Arguments
  * 		reduce: function
- * 			给定当前状态树和要处理的动作的返回下一个状态树的还原函数
- * 		preloadedState: any
- * 			初始状态
- * 			可以选择将其指定为通用应用程序中的服务器状态，或恢复先前序列化的用户会话
- * 			如果使用联合减速器生成减速器，则必须是与传递给它的键具有相同形状的普通对象
+ * 			当前的 state 树和要处理的 action，返回新的 state 树
+ * 		[preloadedState]: any
+ * 			初始时的 state
+ * 			可以决定是否把服务端传来的 state （hydrate）后传给它，或者从之前保存的用户会话中恢复一个传给它
+ * 			如果使用 combineReducers 创建 reducer，它必须是一个普通对象，与传入的 keys 保持同样的结构
  * 			可以自由地通过减速器可以理解的任何事情
  * 		enhancer: function
- * 			可以选择指定它来增强具有第三方功能 applyMiddleware()
+ * 			Store enhancer 是一个组合 store creator 的高阶函数，返回一个新的强化过的 store creator
+ * 			与 middleware 相似，也允许你通过复合函数改变 store 接口
  * 	Returns
  * 		Store:
- * 			一个持有应用程序完整状态的对象
- * 			改变其状态的唯一方法是通过调度行动
- * 			可以订阅其状态更改以更新UI
+ * 			保存了应用所有 state 的对象
+ * 			改变 state 的惟一方法是 dispatch action
+ * 			也可以 subscribe 监听 state 的变化，然后更新 UI
  * */
 
 import { createStore } from 'redux'
+
+console.log('Redux createStore 开始');
 
 function todo(state = [], action) {
 	switch (action.type) {
@@ -33,12 +36,18 @@ function todo(state = [], action) {
 
 let store = createStore(todo, ['use Redux']);
 
+console.log(store.getState());
+//	["use Redux"]
+
 store.dispatch({
 	type: 'ADD_TODO',
 	text: 'Read the docs'
 });
 
 console.log(store.getState());
+//	(2) ["use Redux", "Read the docs"]
+
+console.log('Redux createStore 结束');
 
 class reduxCreateStore extends Component{
 	render() {
---
+++ react-project/src/view/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class footer extends React.Component{
+	render(){
+		return(
+			<div className="ui equal width grid">
+				<div className='column text-center'>
+					footer
+				</div>
+			</div>
+		)
+	}
+}
+
+export default footer;
