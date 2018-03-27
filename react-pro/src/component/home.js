382cb9d3923818f5a1b41ba365a58f769e1b147a
suiyi2016 <suiyi19920228@qq.com>
2018-03-27 13:49:34 +0800

    2018-03-27

--- react-pro/src/component/home.js
+++ react-pro/src/component/home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Progress from './animation/Progress';
+
 class home extends React.Component{
 	render() {
 		return(
 			<div>
+				<Progress/>
 				home
 			</div>
 		)
